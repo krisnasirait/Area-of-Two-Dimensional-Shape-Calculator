3dd3a64f0d7aa5f275f8ce25c59854e5ef7dd77a
Krisna Sirait <krisnapande00@outlook.com>
2022-06-25 21:43:30 +0700

    added bug fix for the two dimensional shape input processing, also make the code more efficient

--- src/Triangle.kt
+++ src/Triangle.kt
@@ -1,4 +1,17 @@
 class Triangle {
+    //area of triangle
+    fun triangleProcessing(){
+        val triangle = Triangle()
+
+        print("Input base\t: ")
+        val base = readLine()!!.toDouble()
+
+        print("Input height\t: ")
+        val height = readLine()!!.toDouble()
+
+        println("Area of triangle\t: " + triangle.areaTriangle(base, height))
+    }
+
     fun areaTriangle(base: Double, height: Double): Double{
         return base * height / 2
     }
--- src/TwoDimensionalShape.kt
+++ src/TwoDimensionalShape.kt
@@ -11,75 +11,14 @@ class TwoDimensionalShape {
        return pilihan
    }
 
-    //area of rectangle
-    fun rectangleProcessing(){
-        val rectangle = Rectangle()
-
-        print("Input length\t: ")
-        var length = readLine()!!.toDouble()
-        print("Input width\t\t: ")
-        var width = readLine()!!.toDouble()
-
-        println("Area of rectangle\t: " + rectangle.areaRectangle(length, width))
-    }
-
-    //area of square
-    fun squareProcessing(){
-        val square = Square()
-
-        print("Input side\t: ")
-        var side = readLine()!!.toDouble()
-
-        println("Area of square\t: " + square.areaSquare(side))
-    }
-
-    //area of circle
-    fun circleProcessing(){
-        val circle = Circle()
-
-        print("Input radius\t: ")
-        var radius = readLine()!!.toDouble()
-
-        println("Area of circle\t: " + circle.areaCircle(radius))
-
-    }
-
-    //area of trapezium
-    fun trapeziumProcessing(){
-        val trapezium = Trapezium()
-
-        print("Input base top\t\t: ")
-        var baseTop = readLine()!!.toDouble()
-
-        print("Input base bottom\t: ")
-        var baseBottom = readLine()!!.toDouble()
-
-        print("Input height\t: ")
-        var height = readLine()!!.toDouble()
-
-        println("Area of trapezium\t: " + trapezium.areaTrapezium(baseTop, baseBottom, height))
-    }
-
-    //area of triangle
-    fun triangleProcessing(){
-        val triangle = Triangle()
-
-        print("Input base\t: ")
-        var base = readLine()!!.toDouble()
-
-        print("Input height\t: ")
-        var height = readLine()!!.toDouble()
-
-        println("Area of triangle\t: " + triangle.areaTriangle(base, height))
-    }
 
     fun pilProcessing(pilihan: Int){
         when(pilihan){
-            1 -> rectangleProcessing()
-            2 -> circleProcessing()
-            3 -> squareProcessing()
-            4 -> triangleProcessing()
-            5 -> trapeziumProcessing()
+            1 -> Rectangle().rectangleProcessing()
+            2 -> Circle().circleProcessing()
+            3 -> Square().squareProcessing()
+            4 -> Triangle().triangleProcessing()
+            5 -> Trapezium().trapeziumProcessing()
         }
     }
 
